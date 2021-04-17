706cb9f03e28647fb1b8d74e980b0c8d36c2ccb8
jillallan <jillallan24@gmail.com>
2021-04-17 18:05:17 +0100

    Update config

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv(path.join(base_dir, '.env'))
 class Config:
     """Set config variables, base config"""
 
-    SECRET_KEY = environ.get('SECRET_KEY')
+    #SECRET_KEY = environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -19,4 +19,5 @@ class DevConfig(Config):
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
+    WTF_CSRF_ENABLED = False
     CSRF_ENABLED = False
